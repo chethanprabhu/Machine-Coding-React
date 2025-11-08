d65fb37b95c6ef02e01d669d7881486702c525cc
Chethan Prabhu <chethanprabhu65@gmail.com>
2025-11-08 16:49:54 +0530

    advanced setup

---
+++ FileExplorer/Advanced/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Explorer from "./components/Explorer";
+import explorerData from "./data/expData";
+
+const App = () => {
+  return <Explorer data={explorerData} />;
+};
+
+export default App;
